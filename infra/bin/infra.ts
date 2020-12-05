653ab78e6641700ed39489769977fe969b442074
medalhkr <medalhkr@gmail.com>
2020-12-05 17:29:32 +0900

    setup s3 bucket

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,5 +3,14 @@ import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { InfraStack } from "../lib/infra-stack";
 
+function env(key: string) {
+  const value = process.env[key];
+  if (value == undefined) console.error(`[ERROR] ENV not found: ${key}`);
+  return value ?? "";
+}
+
 const app = new cdk.App();
-new InfraStack(app, "InfraStack");
+new InfraStack(app, "InfraStack", {
+  bucketName: env("BUCKET_NAME"),
+  allowIps: env("ALLOW_IPS").split(","),
+});
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,9 +1,40 @@
 import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import * as s3 from "@aws-cdk/aws-s3";
+
+type StackParams = {
+  bucketName: string;
+  allowIps: string[];
+};
 
 export class InfraStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    params: StackParams,
+    props?: cdk.StackProps
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "s3Hosting", {
+      bucketName: params.bucketName,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      // httpsにしたいためweb hostingは利用しない。リダイレクトやerror pageが必要であれば使う
+      // websiteIndexDocument: "index.html",
+      // websiteErrorDocument: "error.html",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        principals: [new iam.AnyPrincipal()],
+        actions: ["s3:GetObject"],
+        resources: [`${bucket.bucketArn}/*`],
+        conditions: {
+          IpAddress: {
+            "aws:SourceIp": params.allowIps,
+          },
+        },
+      })
+    );
   }
 }
