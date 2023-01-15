1c1c3700d25029b41dceaf277f4bf8651e10a54a
cumet04 <medalhkr@gmail.com>
2023-01-15 15:45:56 +0900

    test diff (dom)

--- frontend/src/stories/Button.js
+++ frontend/src/stories/Button.js
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label}
+      --{label}--
     </button>
   );
 };
